19086b176fa79bccefcac7f7cf9b583c6cd75b41
Beatriz Martins <biamart2009@gmail.com>
2025-10-18 21:29:30 -0300

    fix: tentativa de deploy com vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/formulario-guilda/',
-  plugins: [react(),
-            tailwindcss()],
-            base: "./",
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: "./", // A propriedade 'base' deve estar aqui, junto com 'plugins'
 })
